3fdb612b0095a9416c558d26a0e95bea34afad48
Ismail Kara <ismail.kara@bilgeadamboost.com>
2022-02-06 17:37:38 +0300

    TestApproaches last Version in TypeLogger_Class()

---
+++ BilgeAdam.TestPractise/Dummy/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeAdam.TestPractise
+{
+    public class Product 
+    {
+        public string Name { get; set; }
+    }
+
+    public interface IProduct
+    {
+
+    }
+}
