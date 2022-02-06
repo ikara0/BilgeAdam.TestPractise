f8245304fb75a015eca61727eab47ea1d606cc4a
Ismail Kara <ismail.kara@bilgeadamboost.com>
2022-02-06 15:10:23 +0300

    Add project files.

---
+++ BilgeAdamNewTest/TestApproaches.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeAdamNewTest
+{
+    internal class TestApproaches
+    {
+        [Test]
+        public void TypeLogger_Class()
+        {
+            var helper = new TypeLogger();
+            var logger = helper.GetTypeProperties<Product>();
+            Assert.AreEqual("BilgeAdam.Product,Product,Class", logger);
+
+        }
+    }
+}
